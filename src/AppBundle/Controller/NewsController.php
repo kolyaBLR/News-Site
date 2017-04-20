4f5b40f440395a226611258702c1275f545e3066
Николай Николаевич Бобров <bk97w@bk.ru>
2017-04-20 17:01:41 +0300

    Описал экшны в контроллере новостей. но не совсем до конца есть пару вопросов.

--- src/AppBundle/Controller/NewsController.php
+++ src/AppBundle/Controller/NewsController.php
@@ -11,10 +11,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\VarDumper\Cloner\Data;
 
 class NewsController extends Controller
 {
-    public function createAction(Request $request)
+    /**
+     * Здесь будет сохраняться уже написаннаня статься
+     * тут расскажешь мне как работать с ролями
+     * что бы ограничить достук юзерам
+     * @Route("/news/create")
+     */
+    public function createNewsAction(Request $request)
     {
         $news = new DataNews(
             '1',
@@ -28,5 +35,39 @@ class NewsController extends Controller
         $create->flush();
 
         return new Response('Saved. ID -> '. $news->getId());
+    }
+
+    /**
+     * Здесь должен выводиться список новостей
+     * на данный момент здесь выводятся все новости
+     * параметры списка новостей:
+     * Заголовок новости и его изображение
+     * так же id для формирвоания ссылки
+     * (Я это пока не могу сделать так как до сих пор мучаюсь с PDO драйвером)
+     * @Route("/news")
+     */
+    public function viewTitleNewsAction(Request $request)
+    {
+        $news = $this->getDoctrine()->getRepository('AppBundle:DataNews')->findAll();
+        $encoders = array(new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+        $newsJson = $serializer->serialize($news, 'json');
+        return new Response(var_dump($newsJson));
+    }
+
+    /**
+     * Здесь будет выводитсья одна стотья по её идентификатору
+     * @Route("/news/{id}")
+     * @ParamConverter("post", class="AppBundle:DataNews")
+     */
+    public function viewNewsAction(DataNews $newsId)
+    {
+        $news = $this->getDoctrine()->getRepository('AppBundle:DataUser')
+            ->find($newsId);
+        if (!$newsId) {
+            throw $this->createNotFoundException('Not found by ID ' .$newsId);
+        }
+        return new Response($news);
     }
 }
